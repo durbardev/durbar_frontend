1b043c1b353244b0caa8aed644db79ca4441ec7d
kawsarmatubbor <kawsarmatubbordec@gmail.com>
2026-06-10 18:37:39 +0600

    add work process

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import Footer from "./components/Footer";
 import Header from "./components/Header";
 import Hero from "./components/Hero";
+import Process from "./components/Process";
 import Services from "./components/Services";
 import TrustedBrands from "./components/TrustedBrands";
 
@@ -15,6 +16,9 @@ function App() {
 
             {/* Trusted Brands */}
             <TrustedBrands />
+
+            {/* Process */}
+            <Process />
 
             {/* Services */}
             <Services />
